0f1da139369618555f485d652cb534d74c775645
juanfana <116502940+juanfana@users.noreply.github.com>
2024-10-23 13:08:17 -0400

    Create Eating.cs

---
+++ Eating.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Eating : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Ant"))
+        {
+            Destroy(other.gameObject);
+        }
+
+    }
+}
