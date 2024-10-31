19a69cd9ed11951f2116e2a4de7113e8304e9f74
@N0rAkFana <116502940+juanfana@users.noreply.github.com>
2024-10-31 17:53:19 -0400

    Update Spawn.cs

--- Spawn.cs
+++ Spawn.cs
@@ -5,15 +5,28 @@ public class Spawn : MonoBehaviour
 {
     public GameObject Ant;
     public Transform spawn;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float Minimum 0.2f;
+    public float Maximum 10f;
+    public float timeUntilSpawn;    
+   //Called at the start of the game
+    void Awake()
     {
-
+      SetTimeUntilSpawn ();
     }
+
+    void Update () 
+    {
+        timeUntilSpawn = Time.deltaTime
+    }
+    
 
     // Update is called once per frame
     void FixedUpdate()
     {
         Instantiate(Ant, spawn.position, quaternion.identity);
+    }
+    public void SetTimeUntilSpawn () 
+    {
+      timeUntilSpawn = Random.Range ( Minimum, Maximum);
     }
 }
