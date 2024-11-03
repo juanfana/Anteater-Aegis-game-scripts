5ce4b69c673a4deb075d43d8c52fc99709938af9
@N0rAkFana <116502940+juanfana@users.noreply.github.com>
2024-11-03 11:09:09 -0500

    Update Action.cs

--- Action.cs
+++ Action.cs
@@ -4,10 +4,11 @@ using System.Collections.Generic;
 
 public class Action : MonoBehaviour
 {
+//Boolean value created for collision detection
     public bool IsTouching; 
-
+//declaring collider that will be used     
      public Collider2D Polygon;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // setting up collider trigger for when certain colliders come in contact with each other
     void Start()
     {
         Polygon = GetComponent<Collider2D>();
@@ -20,7 +21,7 @@ public class Action : MonoBehaviour
 
     }
 
-    // Update is called once per frame
+    // "Eating" button that emulates eating action when coming into contact with enemies
     void FixedUpdate()
     {
         if (Input.GetKeyDown(KeyCode.Space))
