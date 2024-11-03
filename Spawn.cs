f0eb6256c3db7a1b5592dc302894a506c1fa9f0e
@N0rAkFana <116502940+juanfana@users.noreply.github.com>
2024-11-03 11:05:37 -0500

    Update Spawn.cs

--- Spawn.cs
+++ Spawn.cs
@@ -3,28 +3,32 @@ using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
-    public GameObject Ant;
-    public Transform spawn;
-    public float Minimum 0.2f;
-    public float Maximum 10f;
-    public float timeUntilSpawn;    
-   //Called at the start of the game
+//These values can be assigned and tampered with on the editor
+//GameObject the will be spawned
+//Postion where it will be spawned
+//Floats for time between spawns
+    public GameObject;
+    public Transform;
+    public float timeUntilSpawn;
+    public float Minimum;
+    public float Maximum ;    
+   //Used awake instead of start so it can be initialized when the scene first loads
     void Awake()
     {
       SetTimeUntilSpawn ();
     }
-
+    // If statement to check if spawn value reaches zero, allowing the next enemy to spawn
     void Update () 
     {
-        timeUntilSpawn = Time.deltaTime
-    }
-    
+        timeUntilSpawn -= Time.deltaTime
+        if(timeUntilSpawn <=0)
+        {
+          Instantiate(GameObject, transform.position, Quaternion.indentity);
+          SetTimeUntilSpawn ();
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        Instantiate(Ant, spawn.position, quaternion.identity);
+        } 
     }
+    //Method created to give timeUntilSpawn value
     public void SetTimeUntilSpawn () 
     {
       timeUntilSpawn = Random.Range ( Minimum, Maximum);
